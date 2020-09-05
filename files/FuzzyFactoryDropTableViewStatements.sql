db2aa9013c24187501b8c835a69aeec95f7a4cb4
John Walker <johnwalker@Johns-MacBook-Pro.local>
2020-09-05 13:02:46 -0700

    Documenting tables/views associate with dbt that need to be dropped as part of the demo

---
+++ files/FuzzyFactoryDropTableViewStatements.sql
@@ -0,0 +1,13 @@
+drop view if exists mavenfuzzyfactory.stg_sessions;
+drop view if exists mavenfuzzyfactory.stg_orders;
+drop view if exists mavenfuzzyfactory.vw_traffic_bid_optimization_by_device;
+drop view if exists mavenfuzzyfactory.vw_traffic_bid_optimization_by_week;
+drop view if exists mavenfuzzyfactory.vw_session_conversion_rate;
+drop view if exists mavenfuzzyfactory.vw_session_conversion_week_rate_trend;
+drop view if exists mavenfuzzyfactory.vw_landing_page_bounce_rates;
+
+
+drop table if exists mavenfuzzyfactory.stg_first_homepage_pageviews;
+drop table if exists mavenfuzzyfactory.stg_homepage_bounced_sessions;
+drop table if exists mavenfuzzyfactory.tst_vw_session_conversion_rate;
+commit;
