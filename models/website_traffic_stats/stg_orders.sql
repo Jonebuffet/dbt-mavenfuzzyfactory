0eda10c87a20ced786900e0496c265995e0638cf
John Walker <john@chartio.com>
2020-09-11 12:17:55 -0700

    Made stg_orders and stg_sessions incremental

--- models/website_traffic_stats/stg_orders.sql
+++ models/website_traffic_stats/stg_orders.sql
@@ -1,2 +1,14 @@
+
+{{ config(materialized='incremental') }}
+with orders as (
 select *
-from mavenfuzzyfactory.orders 
+from {{ source('mavenfuzzyfactory', 'orders') }}
+
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  where created_at > (select max(created_at) from {{ this }})
+
+{% endif %}
+)
+select * from orders
--- models/website_traffic_stats/stg_sessions.sql
+++ models/website_traffic_stats/stg_sessions.sql
@@ -1,2 +1,14 @@
-select *
-from mavenfuzzyfactory.website_sessions 
+{{ config(materialized='incremental') }}
+with sessions as (
+  select *
+  from {{ source('mavenfuzzyfactory', 'website_sessions') }}
+--  where website_sessions.created_at < '2014-06-14'
+
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  where created_at > (select max(created_at) from {{ this }})
+
+{% endif %}
+)
+select * from sessions
